313002ac5312f0ac791189e84c152bc8f6db8419
MouadAitBouzaid <aitbouzaid.mouad@gmail.com>
2023-02-07 17:39:34 +0100

    first Commit

---
+++ studentService/studentService/DTOs/StudentDTO.cs
@@ -0,0 +1,11 @@
+namespace studentService.DTOs;
+
+public class StudentDTO
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    public string Address { get; set; }
+    public string ContactNumber { get; set; }
+}
